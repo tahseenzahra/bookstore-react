f848c259b9e8b226927d62c8d3226b8ddcf2485a
tahseenzahra <tahseen1zahra@gmail.com>
2022-11-12 16:11:18 +0500

    Resolve display component issue

--- src/components/Book.js
+++ src/components/Book.js
@@ -4,7 +4,7 @@ import { removebook } from '../redux/books/books';
 import BookCSS from '../css-modules/Book.module.css';
 
 const Book = () => {
-  const books = useSelector((state) => state.book);
+  const books = useSelector((state) => state.bookreducer);
   const dispatch = useDispatch();
   const deleteHandler = (id) => {
     dispatch(removebook(id));
@@ -13,6 +13,7 @@ const Book = () => {
     <div className={BookCSS.bookinfo}>
       {books.map((book) => (
         <div key={book.id}>
+          <p>{book.category}</p>
           <p>{book.title}</p>
           <p>{book.author}</p>
           <button type="button" onClick={() => deleteHandler(book.id)}>
